2c297c7a410dbff0907d7f17eeeda9a9ca9bae65
Víctor M <samsaga2@gmail.com>
2021-03-02 09:10:59 +0100

    Added bookmarks

--- init.el
+++ init.el
@@ -14,15 +14,6 @@
 ;; The default is 800 kilobytes.  Measured in bytes.
 (setq gc-cons-threshold (* 50 1000 1000))
 
-;; Profile emacs startup
-(add-hook 'emacs-startup-hook
-          (lambda ()
-            (message "*** Emacs loaded in %s with %d garbage collections."
-                     (format "%.2f seconds"
-                             (float-time
-                              (time-subtract after-init-time before-init-time)))
-                     gcs-done)))
-
 ;; global
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
@@ -159,8 +150,6 @@
     "eb" 'eval-buffer
     "hk" 'describe-key
     "hv" 'describe-variable
-    "fd" 'lsp-find-definition
-    "fr" 'lsp-find-references
     "<SPC>" 'avy-goto-word-or-subword-1))
 
 (use-package undo-tree
@@ -263,10 +252,12 @@
            ;; (require 'ccls)
            (lsp)
            (lsp-lens-mode 1)
-           ;;(lsp-ui-sideline-mode)
-           ;;(lsp-ui-peek-mode)
+           (lsp-ui-sideline-mode)
+           (lsp-ui-peek-mode)
            (origami-mode)
-           )))
+           (evil-leader/set-key
+             "fd" 'lsp-find-definition
+             "fr" 'lsp-find-references))))
 
 (use-package lsp-ui
   :ensure t
@@ -314,12 +305,6 @@
   (sml/setup)
   (sml/apply-theme 'respectful))
 
-;; (use-package mini-modeline
-;;   :ensure t
-;;   :after smart-mode-line
-;;   :config
-;;   (mini-modeline-mode t))
-
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1)
@@ -345,10 +330,6 @@
 	auto-package-update-interval 30
         auto-package-update-hide-results t)
   (auto-package-update-maybe))
-
-;; (use-package clang-format
-;;   :ensure t
-;;   :defer t)
 
 (use-package clang-format+
   :ensure t
@@ -403,6 +384,55 @@
   :mode "\\.vue\\'"
   :config
   (add-hook 'vue-mode-hook #'lsp))
+
+(use-package bm
+  :ensure t
+  :init
+  ;; restore on load (even before you require bm)
+  (setq bm-restore-repository-on-load t)
+
+  :config
+  ;; Allow cross-buffer 'next'
+  (setq bm-cycle-all-buffers t)
+
+  ;; save bookmarks
+  (setq-default bm-buffer-persistence t)
+
+  ;; Loading the repository from file when on start up.
+  (add-hook' after-init-hook 'bm-repository-load)
+  ;; Restoring bookmarks when on file find.
+  (add-hook 'find-file-hooks 'bm-buffer-restore)
+
+  ;; Saving bookmarks
+  (add-hook 'kill-buffer-hook #'bm-buffer-save)
+
+  ;; Saving the repository to file when on exit.  kill-buffer-hook is not called
+  ;; when Emacs is killed, so we must save all bookmarks first.
+  (add-hook 'kill-emacs-hook #'(lambda nil
+                                 (bm-buffer-save-all)
+                                 (bm-repository-save)))
+
+  ;; The `after-save-hook' is not necessary to use to achieve persistence, but
+  ;; it makes the bookmark data in repository more in sync with the file state.
+  (add-hook 'after-save-hook #'bm-buffer-save)
+
+  ;; Restoring bookmarks
+  (add-hook 'find-file-hooks   #'bm-buffer-restore)
+  (add-hook 'after-revert-hook #'bm-buffer-restore)
+
+  ;; The `after-revert-hook' is not necessary to use to achieve persistence, but
+  ;; it makes the bookmark data in repository more in sync with the file
+  ;; state. This hook might cause trouble when using packages that automatically
+  ;; reverts the buffer (like vc after a check-in).  This can easily be avoided
+  ;; if the package provides a hook that is called before the buffer is reverted
+  ;; (like `vc-before-checkin-hook').  Then new bookmarks can be saved before
+  ;; the buffer is reverted.  Make sure bookmarks is saved before check-in (and
+  ;; revert-buffer)
+  (add-hook 'vc-before-checkin-hook #'bm-buffer-save)
+
+  :bind (("<f2>" . bm-next)
+         ("S-<f2>" . bm-previous)
+         ("C-<f2>" . bm-toggle)))
 
 ;; sdz80
 (add-to-list 'load-path "~/.emacs.d/vendors/sdz80-mode")
